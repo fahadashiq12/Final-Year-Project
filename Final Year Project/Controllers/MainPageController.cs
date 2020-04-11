a100072672095923035dc7a11a61df79abbdb6f8
Fahad Ashiq <fahadashiq30@gmail.com>
2020-04-11 09:24:33 +0500

    Theme Implementation

---
+++ Final Year Project/Controllers/MainPageController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Final_Year_Project.Controllers
+{
+    public class MainPageController : Controller
+    {
+        // GET: MainPage
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Final Year Project/Controllers/SigninPageController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Final_Year_Project.Controllers
+{
+    public class SigninPageController : Controller
+    {
+        // GET: SigninPage
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
