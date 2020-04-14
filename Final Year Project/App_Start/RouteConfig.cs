db66271fab7de0e9237fc714670da279b7f951b5
Fahad Ashiq <fahadashiq30@gmail.com>
2020-04-14 22:58:04 +0500

    Front-End Design

--- Final Year Project/App_Start/RouteConfig.cs
+++ Final Year Project/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace Final_Year_Project
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Admin", action = "Index", id = UrlParameter.Optional}
             );
         }
     }
